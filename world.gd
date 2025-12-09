bd519bd2f8000a2e14d684a4e4e62371b2a0c451
BlakeBild <95382901+BlakeBild@users.noreply.github.com>
2025-12-08 22:22:42 -0700

    Rotato!
    the rock will rotate and you have to time it right for the rotation you want

--- world.gd
+++ world.gd
@@ -15,6 +15,9 @@ var angle := 0.00
 var start_action: = false
 var reset_slide := false
 var slide_up := true
+var power_set := false
+var angle_set := false
+var rock_x_rotation_set := false
 
 #What is a comment? A miserable little pile of secrets. But enough talk… Have at you!
 func _ready() -> void:
@@ -26,23 +29,24 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_released("power_hit") and accept_input == true and start_action == false:
 		start_action = true
 		delay_input()
-	if start_action == true and power == 0 and accept_input == true:
+	if start_action == true and power_set == false and accept_input == true:
 		get_power(delta)
-	if start_action == true and power > 0 and accept_input == true:
+	if start_action == true and power_set == true and angle_set == false and accept_input == true:
 		get_angle(delta)
+	if start_action == true and power_set == true and angle_set == true and accept_input == true:
+		get_x_rotation(delta)
 		
 	if Input.is_action_just_released("restart"):
 		get_tree().change_scene_to_file("res://start_screen.tscn")
 	
-	
 	if reset_slide == true:
 		slideNumber = 0
 	
-	if power > 0 and angle > 0 and start_action == true:
+	if power_set == true and angle_set == true and rock_x_rotation_set == true and start_action == true:
 		concept_rock.throw(power, angle)
 		concept_rock.rock_cam_top.current = true
 		start_action = false
-
+		
 
 func get_power(delta):
 	#print(slideNumber)
@@ -52,9 +56,22 @@ func get_power(delta):
 	if Input.is_action_just_released("power_hit") and accept_input == true:
 		power = slideNumber
 		power_amount_label.text = str(power)
+		power_set = true
 		delay_input()
 
-
+func get_x_rotation(delta):
+	var slow_down := 5
+	if slideNumber > 6:
+		slideNumber = 0
+	concept_rock.rock_mesh.rotation.x = slideNumber
+	print(str(concept_rock.rock_mesh.rotation.x))
+	slide_up = slider_direction(slide_up, 1) #1 tells slideNumber to only go as high as 30 instead of 100
+	sliding_numbers(delta/slow_down, slide_up)
+	if Input.is_action_just_released("power_hit") and accept_input == true:
+		concept_rock.rock_mesh.rotation.x = slideNumber
+		#power_amount_label.text = str(power)
+		rock_x_rotation_set = true
+		delay_input()
 
 func get_angle(delta):
 	#print(slideNumber)
@@ -64,12 +81,15 @@ func get_angle(delta):
 	if Input.is_action_just_released("power_hit") and accept_input == true:
 		angle = slideNumber
 		angle_amount_label.text = str(angle)
+		angle_set = true
 		delay_input()
 
 
-func slider_direction(slide_up_value) -> bool:
+func slider_direction(slide_up_value, do_a_rotate:=0) -> bool:
 	if slideNumber < 0:
 		return true
+	elif do_a_rotate == 1 and slideNumber > 5:
+		return false
 	elif slideNumber > 100:
 		return false
 	else:
@@ -77,6 +97,12 @@ func slider_direction(slide_up_value) -> bool:
 
 
 func sliding_numbers(delta, slide_up_value) -> void:
+	if slide_up_value == true:
+		slideNumber = slideNumber + (delta*slider_speed)
+	else:
+		slideNumber = slideNumber + -(delta*slider_speed)
+		
+func sliding_rotate(delta, slide_up_value) -> void:
 	if slide_up_value == true:
 		slideNumber = slideNumber + (delta*slider_speed)
 	else:
